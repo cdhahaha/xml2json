2156add8acf9f19e9d1566f5b68538e55d9ae93d
chend <cdhahaha@163.com>
2018-03-06 17:39:47 +0800

    完善项目

--- src/index.js
+++ src/index.js
@@ -1,18 +1,108 @@
 'use strick'
 
-var sayName = (name) => {
-  return name
+/**
+ * 创建XML文档
+ * @param  {String} strxml XML字符串
+ * @return {Object}        文档对象
+ */
+var createXMLstrDoc = (strxml) => {
+
+  var tmpxmlStrDoc = null
+
+  if (window.DOMParser) {
+    var parser = new DOMParser()
+    tmpxmlStrDoc = parser.parseFromString(strxml, "text/xml")
+  } else {
+    tmpxmlStrDoc = new ActiveXObject("Microsoft.XMLDOM")
+    tmpxmlStrDoc.async = "false"
+    tmpxmlStrDoc.loadXML(strxml)
+  }
+
+  return tmpxmlStrDoc
 }
 
-var sayAge = (age) => {
-  return age
+/**
+ * 获取XML文档
+ * @param  {[type]} xml [description]
+ * @return {[type]}     [description]
+ */
+var getXML = (xml) => {
+
+  var xmlDoc, xmlhttp = new window.XMLHttpRequest()
+
+  xmlhttp.open('GET', xml, true)
+  xmlhttp.send(null)
+
+  return new Promise((resolve, reject) => {
+    xmlhttp.onreadystatechange = () => {
+      if (xmlhttp.status === 200 && xmlhttp.readyState === 4) {
+        xmlDoc = xmlhttp.responseXML ? xmlhttp.responseXML.documentElement : createXMLstrDoc(xmlhttp.responseText).documentElement
+        if (xmlDoc) {
+          resolve(xmlDoc)
+        } else {
+          reject(false)
+        }
+      }
+    }
+  })
 }
 
-var sayColor = (color) => {
-  return color
+/**
+ * xml对象转换为json对象
+ * @param  {String} xmlName XML文件地址
+ * @return {Object}         Promise对象
+ */
+var xml2json = (xmlName) => {
+
+  return new Promise((resolve, reject) => {
+    getXML(xmlName).then((xmlDoc) => {
+
+      var obj = {}
+
+      function mapEle(eles) {
+
+        var result = {}
+
+        Array.from(eles).forEach(ele => {
+
+          var eleObj = {
+            attrs: {}
+          }
+          var attrs = ele.attributes
+          var name = ele.nodeName
+
+          Array.from(attrs).forEach(attr => {
+            eleObj['attrs'][attr.nodeName] = attr.nodeValue
+          })
+
+          if (ele.children.length > 0) {
+            eleObj['children'] = mapEle(ele.children)
+          }
+
+          if (result[ele.nodeName] instanceof Array) {
+            result[ele.nodeName].push(eleObj)
+          } else {
+            result[ele.nodeName] = []
+            result[ele.nodeName].push(eleObj)
+          }
+        })
+
+        return result
+      }
+
+      obj[xmlDoc.nodeName] = mapEle(xmlDoc.children)
+
+      if (!xmlDoc) {
+        resolve(obj)
+      } else {
+        reject(obj)
+      }
+    })
+  })
 }
 
-exports = module.exports = {
-  sayName,
-  sayAge
+if (typeof exports !== 'undefined') {
+  if (typeof module !== 'undefined' && module.exports) {
+    exports = module.exports = xml2json
+  }
 }
